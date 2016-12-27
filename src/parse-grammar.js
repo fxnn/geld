3d202737e3b3f4d7e3c207c71229a20222a07212
Felix Neumann <dev@fxnn.de>
2016-12-27 20:41:32 +0100

    parse-service: Umbau zu Objektorientierung

--- src/parse-grammar.js
+++ src/parse-grammar.js
@@ -1,53 +1,140 @@
-var Mt940Object = function(is, data) {
-    this.is = is;
-    this.data = data;
-}
-Mt940Object.prototype = {
-    assertIs: function () {
-        var expected = Array.prototype.slice.call(arguments);
-        if (expected.indexOf(this.is) < 0) {
-            throw new Error("Was expected to be " + expected + ", but is " + this.is);
-        }
+var Mt940Object = augment.extend(Object, {
+    constructor: function(data) {
+        this.data = data;
     },
-    tag: function () {
-        this.assertIs("fieldname", "fieldsInBraces", "headerblock");
-        if (this.is == "fieldname") {
-            return this.data[2].data[0];
-        }
-        if (this.is == "fieldsInBraces") {
-            return this.data[1];
-        }
-        return this.data[0].data[0];
+});
+Mt940Object.Tag = augment.extend(Mt940Object, {
+    constructor: function(data) {
+        this.uber.constructor.call(this, data);
     },
-    headerdata: function() {
-        this.assertIs("headerblock");
-        return this.data[1];
+    number: function() {
+        return this.data[0];
     },
-    body: function () {
-        this.assertIs("file");
-        return this.data[1];
+});
+Mt940Object.FieldName = augment.extend(Mt940Object, {
+    constructor: function(data) {
+        this.uber.constructor.call(this, data);
+    },
+    tag: function() {
+        return this.data[2];
+    },
+    tagNumber: function() {
+        return this.tag().number();
+    },
+});
+Mt940Object.FieldDataLine = augment.extend(Mt940Object, {
+    constructor: function(data) {
+        this.uber.constructor.call(this, data);
+    },
+    lineContents: function() {
+        return this.data[3];
+    },
+});
+Mt940Object.FieldData = augment.extend(Mt940Object, {
+    constructor: function(lines) {
+        // HINT: data is array of objects, one per line
+        var data = "";
+        for (var i = 0; i < lines.length; i++) {
+            data += lines[i].lineContents();
+        }        
+        this.uber.constructor.call(this, data);
+    },
+    tag: function() {
+        return this.data[2];
+    },
+    tagNumber: function() {
+        return this.tag().number();
+    },
+});
+Mt940Object.Field = augment.extend(Mt940Object, {
+    constructor: function(data) {
+        this.uber.constructor.call(this, data);
     },
     fieldname: function() {
-        this.assertIs("field");
         return this.data[0];
     },
     fielddata: function() {
-        this.assertIs("field");
         return this.data[1].data;
     },
+});
+Mt940Object.Fields = augment.extend(Mt940Object, {
+    constructor: function(data) {
+        this.uber.constructor.call(this, data);
+    },
+    fieldCount: function() {
+        return this.data.length;
+    },
+});
+Mt940Object.FieldsInBraces = augment.extend(Mt940Object, {
+    constructor: function(data) {
+        this.uber.constructor.call(this, data);
+    },
+    tag: function() {
+        return this.data[1];
+    },
+    tagNumber: function() {
+        return this.tag().number();
+    },
+});
+Mt940Object.Body = augment.extend(Mt940Object, {
+    constructor: function(data) {
+        this.uber.constructor.call(this, data);
+    },
     fields: function () {
-        this.assertIs("body");
-        if (this.data.is == "fieldsInBraces") {
+        if (this.data instanceof Mt940Object.FieldsInBraces) {
             var fieldsInBraces = this.data;
             return fieldsInBraces.data[2];
         }
         return this.data;
     },
-    fieldCount: function() {
-        this.assertIs("fields");
-        return this.data.length;
+});
+Mt940Object.HeaderContent = augment.extend(Mt940Object, {
+    constructor: function(data) {
+        this.uber.constructor.call(this, data);
     },
-};
+    tag: function() {
+        return this.data[0];
+    },
+    tagNumber: function() {
+        return this.tag().number();
+    },
+    headerData: function() {
+        return this.data[1];
+    },
+});
+Mt940Object.HeaderBlock = augment.extend(Mt940Object, {
+    constructor: function(data) {
+        this.uber.constructor.call(this, data);
+    },
+    headerContent: function() {
+        return this.data;
+    },
+    tag: function() {
+        return this.headerContent().tag();
+    },
+    tagNumber: function() {
+        return this.tag().number();
+    },
+    headerData: function() {
+        return this.headerContent().headerData();
+    }
+});
+Mt940Object.Header = augment.extend(Mt940Object, {
+    constructor: function(data) {
+        this.uber.constructor.call(this, data);
+    },
+    headerBlocks: function() {
+        return this.data;
+    }
+});
+Mt940Object.File = augment.extend(Mt940Object, {
+    constructor: function(data) {
+        this.uber.constructor.call(this, data);
+    },
+    body: function() {
+        return this.data[1];
+    }
+});
 
 var Mt940LogLevel = {
     None: {value:0, logExceptions:false, logParsingSteps:false},
@@ -64,39 +151,37 @@ var Mt940Grammar = function (o, logLevel) {
     this.o = o;
 };
 Mt940Grammar.prototype = {
-    _isLogExceptions: function() {
-        return this.logLevel.logExceptions;
+    $token: function(name, regex) {
+        this[name] = this.o.stoken(regex);
     },
-    _isLogParsingSteps: function() {
-        return this.logLevel.logParsingSteps;
-    },
-    // flatten(a) flattens an array a of arrays
-    // http://stackoverflow.com/a/16953805
-    _flatten: function(a, r){
-        if(!r){ r = []}
-        for(var i=0; i<a.length; i++){
-            if(a[i].constructor == Array){
-                this._flatten(a[i], r);
-            }else{
-                r.push(a[i]);
-            }
+    $rule: function(name, rule) {
+        if (rule === null) {
+            throw new Error("Rule for " + name + " is null!");
         }
-        return r;
+        this[name] = this._process(name, rule, function(data) {
+            return this._createMt940Object(name, data);
+        });
     },
-    // concatToString(a) concats all elements in array a to one string
-    _concatToString: function(a) {
-        var result = "";
-        for(var i = 0; i <a.length; i++) {
-            if (a[i] == null) {
-                continue;
-            }
-            if (Object.keys(a[i]).indexOf("data") >= 0) {
-                result = result + a[i].data;
-            } else {
-                result = result + a[i];
-            }
-        }
-        return result;
+    _createMt940Object: function(name, data) {
+        if (name == "file") return new Mt940Object.File(data);
+        if (name == "body") return new Mt940Object.Body(data);
+        if (name == "header") return new Mt940Object.Header(data);
+        if (name == "headerblock") return new Mt940Object.HeaderBlock(data);
+        if (name == "headercontent") return new Mt940Object.HeaderContent(data);
+        if (name == "fields") return new Mt940Object.Fields(data);
+        if (name == "fieldsInBraces") return new Mt940Object.FieldsInBraces(data);
+        if (name == "field") return new Mt940Object.Field(data);
+        if (name == "fieldname") return new Mt940Object.FieldName(data);
+        if (name == "fielddata") return new Mt940Object.FieldData(data);
+        if (name == "fielddataline") return new Mt940Object.FieldDataLine(data);
+        if (name == "tag") return new Mt940Object.Tag(data);
+        return new Mt940Object(data);
+    },
+    // Mimics parser-generator.js's process function and adds custom behaviour.
+    _process: function(name, rule, processor) {
+        var fn = this.o.process(rule, processor);
+        fn.ruleName = name;
+        return this._handleErrors(fn);
     },
     // NOTE: parser-generator.js has bad error handling, as there's no stacktrace whatsoever.
     // We add this behaviour using this decorator function.
@@ -119,35 +204,10 @@ Mt940Grammar.prototype = {
             }
         }
     },
-    // Mimics parser-generator.js's process function and adds custom behaviour.
-    _process: function(name, rule, processor) {
-        var fn = this.o.process(rule, processor);
-        fn.ruleName = name;
-        return this._handleErrors(fn);
+    _isLogExceptions: function() {
+        return this.logLevel.logExceptions;
     },
-    $token: function(name, regex) {
-        this[name] = this.o.stoken(regex);
+    _isLogParsingSteps: function() {
+        return this.logLevel.logParsingSteps;
     },
-    $rule: function(name, rule) {
-        if (rule === null) {
-            throw new Error("Rule for " + name + " is null!");
-        }
-        this[name] = this._process(name, rule, function(data) {
-            return new Mt940Object(name, data);
-        });
-    },
-    $inline: function(name, rule) {
-        this[name] = this._process(name, rule, function(inlined) {
-            if (Object.keys(inlined).indexOf("data") >= 0) {
-                return new Mt940Object(name, inlined.data);
-            }
-            throw new Error("Cannot inline " + JSON.stringify(inlined));
-        });
-    },
-    $concat: function(name, rule) {
-        var _concatToString = this._concatToString;
-        this[name] = this._process(name, rule, function(a) {
-            return new Mt940Object(name, _concatToString(a));
-        });
-    }
 };
